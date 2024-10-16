af4c3600f54e004cc7b49c498e5c4e36ff6377b4
sakuraaasd <zhangke970424@gmail.com>
2024-10-16 21:35:49 +0900

    python 10.15 study

---
+++ python_10.15.py
@@ -0,0 +1,98 @@
+# sum = 0
+# i = 1
+# while i < 100:
+#     sum += i
+#     i += 1
+# print(f'1-100: {sum} ')
+
+# import random
+# num = random.randint(1,100)
+
+# flag = True
+# while flag:
+#     gusse_num = int(input('请输入一个数字: '))
+#     if gusse_num == num:
+#         print('Right!')
+#         flag = False
+#     else:
+#         if gusse_num > num:
+#             print('Big!')
+#         else:
+#             print('small')
+
+# i = 1
+# while i <= 100:
+#     print(f'Today is {i} day,i will ,,,')
+#     j = 1 
+#     while j <= 10:
+#         print(f'rose{j}')
+#         j += 1 
+#     print('meili i like you,,')
+#     i += 1
+# print(f'insit{i-1}day,,success! ')
+
+# i = 1 
+# while i <= 9:
+#     j = 1
+#     while j <= i:
+#         print(f'{j}*{i} = {j*i}\t')
+#         j += 1
+#     i += 1
+#     print()
+
+# name =  'i t it heima'
+# count = 0
+# for x in name:
+#     if x == 'i':
+#         count += 1
+# print(f'Have i {count} ')
+
+# for x in range(10):
+#     print(x)
+
+# ou = 0
+# for x in range(100):
+#     if x % 2 == 0:
+#          ou += 1
+# print(f'ou have {ou}')
+
+# x = 0
+# for x in range(1,101):
+#     print(f'i said {x}day')
+#     for y in range(1,11):
+#         print(f'give {y} flavor')
+#     print(f'like {x} day')
+# print(f'{x}day success')
+
+
+# count = 10000
+# for x in range(1,21):
+#     import random
+#     num = random.randint(1,10)
+#     if num < 5 :
+#         print(f'{x} {num} is low, so no money')
+#         continue
+
+#     if count != 0 :
+#         print(f'give {x} money')
+#         count_1 = count - 1000 
+#     if count == 0 :
+#         print('no money')
+#         break
+
+# count = 10000
+# for x in range(1,21):
+#     import random
+#     num = random.randint(1,10)
+#     if num < 5:
+#         print(f'{x}\'s {num} is low ')
+#         continue
+
+#     if count >= 1000:
+#         count -= 1000
+#         print(f'{x} got 1000 yuan ')
+#     else:
+#         print(f'only {count}yuan. no money')
+#         break
+
+
--- study_python.py
+++ study_python.py
@@ -1,3 +1,56 @@
+"""
 print('hellow world')
 practice = 'python'
 print(practice)
+# これはコメントです
+# これはコーディングのスタイルです
+def a():
+    print('hellow')
+    print('hellow')
+    print('hellow')
+    print('hellow')
+    return 3
+print(a())
+
+a = 1
+type(a)
+print(type(a))
+a = "aaa" #これもコーディングのスタイルです
+"""
+def add(a,b):
+    return a + b
+add(1,2)
+print(add(1,2))
+
+def a():
+    print('hellow')
+    print('hellow')
+    print('hellow')
+    print('hellow')
+    return 3
+a()
+
+b = 10%3.0
+print(b)
+
+x = 1
+y = 10
+c = x/y
+print(c)
+
+a = 1
+b = 1.0 
+d = a + b
+print(d)
+
+a = 'test'
+a[0]
+print(a[0])
+len(a)
+print(len(a))
+
+a = '1'
+b = '2'
+a + b 
+print(a + b)
+
