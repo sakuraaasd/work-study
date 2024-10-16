f2264535f70e2cdeb1489074368892d921989a52
sakuraaasd <zhangke970424@gmail.com>
2024-10-16 23:53:43 +0900

    python_study_10.16(2)

--- python_10.16.py
+++ python_10.16.py
@@ -2,17 +2,28 @@
 #     print(f'姓名是： {name},年龄是: {age}, 性别是: {gender}')
 # user_info(name='Any',age=20,gender='男')
 
-def test_func(computer):
-    result = computer(1,2)
-    print(f'type{type(computer)}')
-    print(f'{result}')
+# def test_func(computer):
+#     result = computer(1,2)
+#     print(f'type{type(computer)}')
+#     print(f'{result}')
 
-def computer(x,y):
-    return x + y
+# def computer(x,y):
+#     return x + y
 
-test_func(computer)
+# test_func(computer)
 
-def add(x,y):
-    return x + y
+# def add(x,y):
+#     return x + y
 
-test_func(add)
+# test_func(add)
+
+#函数指针
+# def add(x,y):
+#     print(x + y)
+# add(1,2)
+# print(add)
+# b = add
+# b(2,2)
+# def temp(x,y,fun):
+#     fun(x,y)
+# temp(3,2,add)
