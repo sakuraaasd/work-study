91aef51c9c4c1bd1ff3634c29f812a13051dee75
チョウ カ <choka@fsi.co.jp>
2024-10-17 11:40:58 +0900

    add 10.17

---
+++ python_study/python_10.17.py
@@ -0,0 +1,12 @@
+# #文件
+# f = open("C:\\Users\\choka\\Desktop\\study\\way.txt",'r',encoding='UTF-8')
+# print(type(f))
+
+# print(f.read())
+
+
+f = open("C:\\Users\\choka\\Desktop\\study\\test.txt",'a',encoding='UTF-8')
+
+f.write('sakuraaasd')
+f.flush()
+f.close
