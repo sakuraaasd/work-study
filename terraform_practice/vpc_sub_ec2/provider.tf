0e7a6706a44ccc856f30b9f962ce4cf70ca9c717
sakuraaasd <zhangke970424@gmail.com>
2024-12-06 16:28:38 +0900

    20241206 pm 1628 commit

--- terraform_practice/vpc_sub_ec2/provider.tf
+++ terraform_practice/vpc_sub_ec2/provider.tf
@@ -1,9 +1,14 @@
+terraform {
+  required_version = ">=0.12"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.80.0"
+    }
+  }
+}
 provider "aws" {
     access_key = var.ACCESS_KEY
     secret_key = var.SECRET_KEY
     region = var.AWS_REGION
-}
-
-terraform {
-  required_version = ">=0.12"
 }
